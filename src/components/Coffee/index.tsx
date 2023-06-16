a5ed0e4d2feefe5b23559a9414c5e6d99f3cebaf
joa0-pedro <joaopedroamorim56@gmail.com>
2023-06-16 15:33:14 -0300

    update project

---
+++ src/components/Coffee/index.tsx
@@ -0,0 +1,7 @@
+export function CoffeeCard(){
+	return (
+		<div>
+			
+		</div>
+	)
+}
--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,47 +1,55 @@
-import {CurrencyDollar, MapPinLine} from 'phosphor-react'
-import { AddressForm, Cards, FormCard, InputBlock, PaymentCheckout, PaymentMethodCard, PaymentMethodTitle, PaymentTitle, Title } from './styles'
-import { BaseInput } from './components/Input'
-import { PaymentMethodButton } from './components/PaymentButton'
-import { CheckoutCard } from './components/CheckoutCard'
+import { CurrencyDollar, MapPinLine } from "phosphor-react";
+import {
+  AddressForm,
+  Cards,
+  FormCard,
+  InputBlock,
+  PaymentCheckout,
+  PaymentMethodCard,
+  PaymentMethodTitle,
+  PaymentTitle,
+  Title,
+} from "./styles";
+import { BaseInput } from "./components/Input";
+import { PaymentMethodButton } from "./components/PaymentButton";
+import { CheckoutCard } from "./components/CheckoutCard";
+import React from "react";
 
-export function Payment(){
-	return (
-			<FormCard id="Form Card">
-				<h3>
-				Complete seu pedido
-				</h3>
-				<Cards>
-				<AddressForm id="formulário">
-					<PaymentTitle id="Form title">
-						<MapPinLine size={22} />
-						<Title>
-							Endereço de Entrega
-							<h5>Informe o endereço onde deseja receber seu pedido</h5>
-						</Title>
-					</PaymentTitle>
-					<InputBlock>
-						<BaseInput />
-					</InputBlock>
-				</AddressForm>
+export function Payment() {
+  return (
+    <FormCard id="Form Card">
+      <h3>Complete seu pedido</h3>
+      <Cards>
+        <AddressForm id="formulário">
+          <PaymentTitle id="Form title">
+            <MapPinLine size={22} />
+            <Title>
+              Endereço de Entrega
+              <h5>Informe o endereço onde deseja receber seu pedido</h5>
+            </Title>
+          </PaymentTitle>
+          <InputBlock>
+            <BaseInput />
+          </InputBlock>
+        </AddressForm>
 
-				<PaymentCheckout>
-					<CheckoutCard />
-				</PaymentCheckout>
-				</Cards>
+        <PaymentCheckout>
+          <CheckoutCard />
+        </PaymentCheckout>
+      </Cards>
 
-				<PaymentMethodCard>
-					<PaymentMethodTitle>
-						<CurrencyDollar size={22}/>
-						<Title>
-							Pagamento
-							<h5>O pagamento é feito na entrega. Escolha a forma que deseja pagar</h5>
-						</Title>
-					</PaymentMethodTitle>
-					<PaymentMethodButton />
-				</PaymentMethodCard>
-
-
-
-			</FormCard>
-	)
+      <PaymentMethodCard>
+        <PaymentMethodTitle>
+          <CurrencyDollar size={22} />
+          <Title>
+            Pagamento
+            <h5>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </h5>
+          </Title>
+        </PaymentMethodTitle>
+        <PaymentMethodButton />
+      </PaymentMethodCard>
+    </FormCard>
+  );
 }
